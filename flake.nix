4481d9c9de39b30707d716d7d1e0a28baa947e0c
Muhammad Sabeeh <muhammadsabeeh01@gmail.com>
2024-09-29 01:05:30 -0400

    structure

--- flake.nix
+++ flake.nix
@@ -4,14 +4,10 @@
   };
   outputs = { self, nixpkgs, ... }@inputs: {
     # replace 'joes-desktop' with your hostname here.
-    myModules = import ./modules { inherit nixpkgs; };
-
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs; };
       system = "x86_64-linux";
       modules = [ 
         ./configuration.nix
-        self.myModules
       ];
     };
   };
