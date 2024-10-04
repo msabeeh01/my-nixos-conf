455a37c6c4ef27628fd8a5885f3882d7cf861412
Sabeeh <muhammadsabeeh01@gmail.com>
2024-10-03 23:01:12 -0400

    fhs-shell changes

--- shell.nix
+++ shell.nix
@@ -1,37 +1,18 @@
-{
-  pkgs ? import <nixpkgs> {
-    config.allowUnfree = true;
-  },
-}:
-let fhs = pkgs.buildFHSEnv {
+{ pkgs ? import <nixpkgs> {} }:
+
+(pkgs.buildFHSEnv {
   name = "simple-x11-env";
-  targetPkgs =
-    pkgs:
-    (with pkgs; [
-      udev
-      alsa-lib
-    ])
-    ++ (with pkgs.xorg; [
-      libX11
-      libXcursor
-      libXrandr
-      #software
-    ]);
-  multiPkgs =
-    pkgs:
-    (with pkgs; [
-      udev
-      alsa-lib
-    ]);
+  targetPkgs = pkgs: (with pkgs; [
+    udev
+    alsa-lib
+  ]) ++ (with pkgs.xorg; [
+    libX11
+    libXcursor
+    libXrandr
+  ]);
+  multiPkgs = pkgs: (with pkgs; [
+    udev
+    alsa-lib
+  ]);
   runScript = "bash";
-
-  };
-in pkgs.mkShell {
-    buildInputs =[
-      fhs
-    ];
-  nativeBuildInputs = [
-    fhs
-  ];
-}
-
+}).env
