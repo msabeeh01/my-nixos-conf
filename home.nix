ff65ac7e0ae7b0f9b332376ed869ac705963363e
Sabeeh <muhammadsabeeh01@gmail.com>
2024-10-10 16:55:21 -0400

    code cleanup

--- home.nix
+++ home.nix
@@ -13,7 +13,6 @@
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
-  #
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
@@ -36,6 +35,8 @@
     android-studio
     bruno
     inkscape-with-extensions
+
+    #Viewer for proxmox
     virt-viewer
 
     #neovim dependencies
